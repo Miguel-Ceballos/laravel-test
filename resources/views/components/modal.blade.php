87f1c2dcca81d4ad81de2541babe2c16780d650c
Miguel Ceballos <mceballosleonides@gmail.com>
2025-01-16 22:23:29 -0600

    agrega efecto de glass a modal

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -23,14 +23,14 @@ $maxWidth = [
     class="jetstream-modal fixed inset-0 overflow-y-auto px-4 py-6 sm:px-0 z-50"
     style="display: none;"
 >
-    <div x-show="show" class="fixed inset-0 transform transition-all" x-transition:enter="ease-out duration-300"
+    <div x-show="show" class="fixed inset-0 transform transition-all backdrop-blur-sm" x-transition:enter="ease-out duration-300"
 {{--    <div x-show="show" class="fixed inset-0 transform transition-all" x-on:click="show = false" x-transition:enter="ease-out duration-300"--}}
                     x-transition:enter-start="opacity-0"
                     x-transition:enter-end="opacity-100"
                     x-transition:leave="ease-in duration-200"
                     x-transition:leave-start="opacity-100"
                     x-transition:leave-end="opacity-0">
-        <div class="absolute inset-0 bg-gray-900 opacity-50"></div>
+        <div class="absolute inset-0 bg-gray-500/30"></div>
     </div>
 
     <div x-show="show" class="mb-6 bg-white rounded-lg overflow-hidden shadow-xl transform transition-all sm:w-full {{ $maxWidth }} sm:mx-auto"
